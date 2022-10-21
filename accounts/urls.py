6abce150604b7afb97043cc5d09b34f389cf6d31
yuvraj <yuvrajsaxena2015@gmail.com>
2022-10-21 11:29:26 +0530

    server setup

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from . import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home),
+    path('products/',views.products),
+    path('customer/',views.customer)
+]
