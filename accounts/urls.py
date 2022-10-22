503d4e5e603555af1ece9304643de44f2f598d4a
yuvraj <yuvrajsaxena2015@gmail.com>
2022-10-22 19:01:40 +0530

    update links and buttons on dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
-    path('products/',views.products),
-    path('customer/<str:pk_test>/',views.customer)
+    path('',views.home, name="home"),
+    path('products/',views.products, name="products"),
+    path('customer/<str:pk_test>/',views.customer, name="customer"),
 ]
