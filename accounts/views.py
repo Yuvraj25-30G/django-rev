7677fa804fbc2e30b633dfb75af29a0ab754e4a8
yuvraj <yuvrajsaxena2015@gmail.com>
2022-10-22 18:28:11 +0530

    update dashboard by rendering data to templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from matplotlib.style import context
+from .models import *
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customers.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+
+    delivered = orders.filter(status = 'Delivered').count()
+    
+    pending = orders.filter(status = 'PENDING').count()
+
+    context = {'orders': orders, 'customers': customers, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+
+    return render(request,'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products = Product.objects.all()
+    return render(request,'accounts/products.html',{'products':products})
 
 
 def customer(request):
