5870ef1bb7823023bc340c8a187df51e6af6d0da
yuvraj <yuvrajsaxena2015@gmail.com>
2022-11-24 02:37:44 +0530

    add r tests to verify the functioning of register page

---
+++ accounts/tests/test_view.py
@@ -0,0 +1,29 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from accounts.models import *
+import json
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.register_url = reverse('register')
+        self.user = {
+            'username': 'username',
+            'email': 'test@gmail.com',
+            'password': 'password',
+            'password2': 'password',
+        }
+        return super().setUp()
+
+class TestRegister(BaseTest):
+    def test_can_view_register_page(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)# test1: check if the url page is visible 
+        self.assertTemplateUsed(response,'accounts/register.html')# test2: check if correct templlate is used
+        print("running1")
+
+
+    #test to se if the user can register themselves or not
+    def test_user_can_register(self):
+        response = self.client.post(self.register_url,self.user,format='text/html')
+        self.assertEqual(response.status_code, 200)
+        print("running2")
